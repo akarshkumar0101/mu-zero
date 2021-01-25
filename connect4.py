c6e1d1e516156aa451fc9e5c2ee2eb774992b7a3
Akarsh Kumar <akarshkumar0101@gmail.com>
2021-01-24 21:00:47 -0600

    organize code and update structure of MCTS
    This is not fully functional code yet.

--- connect4.py
+++ connect4.py
@@ -1,15 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+import collections
+State = collections.namedtuple('State', ['board', 'player'])
 
-def init_state(rows=6, columns=7):
-    return np.zeros((rows, columns), dtype=np.int8)
+def init_state(rows=6, columns=7, player=1):
+    """
+    Returns the initial state given the hyperparameters and the first player.
+    """
+    board = np.zeros((rows, columns), dtype=np.int8)
+    return State(board, player)
+
 def get_actions(state):
-    return np.where((state==0).any(axis=0))[0]
+    """
+    Returns possible actions from a state as an array.
+    """
+    return np.where((state.board==0).any(axis=0))[0]
 
 def render(state, mode='img'):
+    """
+    Renders the state in either 'img' or 'txt' mode.
+    """
     if mode=='img':
-        plt.imshow(state*2./3., vmin=-1, vmax=1, cmap='seismic')
+        plt.imshow(state.board*2./3., vmin=-1, vmax=1, cmap='seismic')
+        plt.title(f'{state.player} to move')
         plt.colorbar()
         plt.ylim(-0.5, 5.5)
         plt.xticks(np.arange(7)+.5)
@@ -21,35 +35,53 @@ def render(state, mode='img'):
             for c in range(6):
                 print('X|' if state[r, c]==1 else ('O|' if state[r,c]==-1 else ' |'), end='')
             print()
-        
 
-def take_action(state, action, player):
-    row = np.where(state[:, action]==0)[0].min()
-    ns = state.copy()
-    ns[row, action] = player
+
+def take_action(state, action):
+    """
+    Takes the given action from the state and returns the new state.
+    """
+    row = np.where(state.board[:, action]==0)[0].min()
+    ns = State(state.board.copy(), -state.player)
+    ns.board[row, action] = state.player
     return ns
 
 def calc_winner(state):
-    win = state[:, :-3]+state[:, 1:-2]+state[:, 2:-1]+state[:, 3:]
+    """
+    Calculate the winner of the game from the state.
+    Will return either 1, -1, or 0 (draw).
+    """
+    win = state.board[:, :-3]+state.board[:, 1:-2]+state.board[:, 2:-1]+state.board[:, 3:]
     if (np.abs(win)==4).any():
         return np.sign(win[np.where(np.abs(win)==4)])[0]
 
-    win = state[:-3, :]+state[1:-2, :]+state[2:-1, :]+state[3:, :]
+    win = state.board[:-3, :]+state.board[1:-2, :]+state.board[2:-1, :]+state.board[3:, :]
     if (np.abs(win)==4).any():
         return np.sign(win[np.where(np.abs(win)==4)])[0]
 
-    win = state[:-3, :-3]+state[1:-2, 1:-2]+state[2:-1, 2:-1]+state[3:, 3:]
+    win = state.board[:-3, :-3]+state.board[1:-2, 1:-2]+state.board[2:-1, 2:-1]+state.board[3:, 3:]
     if (np.abs(win)==4).any():
         return np.sign(win[np.where(np.abs(win)==4)])[0]
 
-    win = state[3:, :-3]+state[2:-1, 1:-2]+state[1:-2, 2:-1]+state[:-3, 3:]
+    win = state.board[3:, :-3]+state.board[2:-1, 1:-2]+state.board[1:-2, 2:-1]+state.board[:-3, 3:]
     if (np.abs(win)==4).any():
         return np.sign(win[np.where(np.abs(win)==4)])[0]
     return 0
 
 def is_draw(state):
-    return (state!=0).all()
+    """
+    Returns boolean if the game is a draw.
+    """
+    return (state.board!=0).all()
 
 def is_done(state):
+    """
+    Returns a tuple (done, winner) for the state.
+    `done` is true if the game is over.
+    `winner` marks the winner by player 1, -1, or 0 (draw)
+    """
     winner = calc_winner(state)
     return is_draw(state) or winner != 0, winner
+
+
+
